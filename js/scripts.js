429dde2de8c9cc613a8390d0a4ce003f97a09752
Ewa Perzanowska <ewaperzanowska5@gmail.com>
2016-09-23 18:31:14 +0200

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var womensNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var mensNames = ['Piotrek', 'Marek', 'Arek', 'Jarek']
+
+var allNames = womensNames.concat(mensNames);
+
+console.log(allNames);
+
+var newName = 'Marian'
+
+if (allNames.indexOf(newName) == -1) {
+
+	var x = allNames.push(newName)
+
+	console.log(allNames);
+
+} 
+
+else if (allNames.indexOf(newName) != -1) {
+
+	console.log('Imię nie może się powtarzać! Imię ' + newName + ' już jest w zestawieniu')
+}
